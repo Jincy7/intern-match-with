b0995c388fce887257dbf318fd4cf249984e5d14
Huh Jin-Ho <huhjin-ho@Huhui-MacBookPro.local>
2019-12-08 16:18:37 +0900

    add student apply

--- src/components/Pages/Applicant.vue
+++ src/components/Pages/Applicant.vue
@@ -7,45 +7,47 @@
                 </h1>
             </header>
             <div class="home-container">
-                <sui-card-group :items-per-row="3" stackable>
-                    <sui-card>
-                        <sui-image :src="image[0]"/>
+                <sui-card class="apply-list">
+                    <sui-card-content>
+                        <sui-card-header>지원자 목록</sui-card-header>
+                        <sui-card-meta>기업에서 진행중인 인턴 공고입니다!</sui-card-meta>
+                        <sui-list divided relaxed>
+                            <sui-list-item :key="info" v-for="info in companyInfo[0].applicants">
+                                <sui-list-content>
+                                    <a v-if="!info.isChecked" is="sui-list-header" v-on:click="loadModal(info.id)">{{info.name}}</a>
+                                    <sui-button v-if="!info.isChecked" positive content="합격" />
+                                    <sui-button v-if="!info.isChecked" negative content="불합격" />
+                                </sui-list-content>
+                            </sui-list-item>
+                        </sui-list>
+                    </sui-card-content>
+                </sui-card>
+                <sui-modal v-model="modal">
+                <sui-modal-header>{{internInfo[modalId].internName}} 질문</sui-modal-header>
+                <sui-modal-content>
+                    <sui-card class="apply-list">
                         <sui-card-content>
-                            <sui-card-header>씹창놈</sui-card-header>
-                            <sui-card-meta>취업 성공하고 면접자들 엿먹이려고 지원함</sui-card-meta>
-                        </sui-card-content>
-                        <sui-card-content extra>
-                            <sui-icon name="users" /> 여자친구 3 Girls
-                        </sui-card-content>               
-                    </sui-card>
-                    <sui-card>
-                        <sui-image :src="image[3]"/>
-                        <sui-card-content>
-                            <sui-card-header>곧 28살</sui-card-header>
-                            <sui-card-meta>취업 성공하고 면접자들 엿먹이려고 지원함</sui-card-meta>
-                        </sui-card-content>
-                        <sui-card-content extra>
-                            <sui-icon name="users" /> 여자친구 20 Girls
-                        </sui-card-content>
-                    </sui-card>
-                    <sui-card>
-                        <sui-image :src="image[2]"/>
-                        <sui-card-content>
-                            <sui-card-header>환승역 스캐너</sui-card-header>
-                            <sui-card-meta>창업 성공하고 면접자들 엿먹이려고 지원함</sui-card-meta>
-                        </sui-card-content>
-                        <sui-card-content extra>
-                            <sui-icon name="users" /> 비서 3 Girls
-                        </sui-card-content>
-                    </sui-card>
-                    <sui-card>
-                        <sui-image :src="image[1]"/>
-                        <sui-card-content>
-                            <sui-card-header>간잽이</sui-card-header>
-                            <sui-card-meta>지원할까 말까~</sui-card-meta>
+                            <sui-list divided relaxed>
+                                <sui-list-item :key="ques" v-for="ques in internInfo[modalId].questions">
+                                    <sui-list-icon size="large" vertical-align="middle"/>
+                                    <sui-list-content>
+                                        <a is="sui-list-header">{{ques}}</a>
+                                        <div class="ui form">
+                                        <div class="field">
+                                            <textarea></textarea>
+                                        </div>
+                                        </div>
+                                    </sui-list-content>   
+                                </sui-list-item>
+                            </sui-list>
                         </sui-card-content>
                     </sui-card>
-                </sui-card-group>              
+                </sui-modal-content>
+                <sui-modal-actions>
+                    <sui-button primary @click.native="toggle2">제출하기</sui-button>
+                    <sui-button secondary @click.native="toggle2">취소</sui-button>
+                </sui-modal-actions>
+            </sui-modal>
             </div>
         </div>
     </v-fragment>
@@ -54,9 +56,66 @@
 <script>
 export default {
     name: "Applicant",
-    data: function () {
+    methods: {
+            loadModal(id) {
+                this.modalId = id
+                this.modal = !this.modal
+            },
+            toggle1() {
+                this.modal = !this.modal
+            },
+            checkToggle() {
+                this.modal2 = !this.modal2
+                this.modal1 = false
+            },
+    },
+    data: () => {
         return {
-            image: []
+            modalId: 0,
+            modal: false,
+            companyInfo: [
+                {
+                        id: 0,
+                        imageSrc: require(`@/../public/images/Samsung.png`),
+                        header: `삼성`,
+                        desc: `Samsung`,
+                        applicants:[
+                            {
+                                name: '허진호',
+                                isChecked: false,
+                                answers: [
+                                    'ddddd',
+                                    'dddddd',
+                                    'fdfdfsf'
+                                ]
+                            },
+                            {
+                                name: '진창엽',
+                                isChecked: false,
+                                answers: [
+                                    'ddddd',
+                                    'dddddd',
+                                    'fdfdfsf'
+                                ]
+                            }
+                        ]
+                },
+                {
+                        id: 1,
+                        imageSrc: require(`@/../public/images/Naver.jpg`),
+                        header: `네이버`,
+                        desc: `Naver`,
+                        internIds:[0]
+                },
+                {
+                        id: 2,
+                        imageSrc: require(`@/../public/images/Kakao.png`),
+                        header: `카카오`,
+                        desc: `KaKao`,
+                        internIds:[2]
+                },
+            ],
+                
         }
     },
     components: {
